724bdf7a02203eb8dac0ae189041c247d16d4553
David Reyes-Cairo(SurfaceBook) <dreyescairo@gmail.com>
2017-11-01 17:41:24 -0600

    various style and animation tweaks. added player health and a message if won or lost.

--- Capstone/js/main.js
+++ Capstone/js/main.js
@@ -6,12 +6,15 @@ var chosenWord = "";
 var wordLength = 0;
 var underScoreArray = [];
 var enemyHealth = 0;
+var playerHealth = 0;
+var enemy = document.getElementById('enemy');
 
 function initGame() {
 
     chosenWord = getRandomWord();
     wordLength = chosenWord.length;
-    enemyHealth = 6;
+    enemyHealth = wordLength;
+    playerHealth = wordLength - 1;
 
     console.log(chosenWord);
 
@@ -20,6 +23,8 @@ function initGame() {
     }
     document.getElementById('letterPlaceholder').innerHTML = underScoreArray.join("  ");
     document.getElementById('health').innerHTML = 'Enemy Health: ' + enemyHealth;
+
+    document.getElementById('playerHealth').innerHTML = 'Your Health: ' + playerHealth;
 }
 
 
@@ -41,25 +46,29 @@ function checkUserInput(letter) {
     var foundCharacter = false;
     var currentTile = document.getElementById(letter);
     var placeholder = document.getElementById('letterPlaceholder');
-    placeholder.style.color = "black"; //reset the color back to default at the beginning of function call.
+    placeholder.style.color = "white"; //reset the color back to default at the beginning of function call.
     resetEnemyVisualDefault();
 
 
     letter = letter.toLowerCase();
 
-    if (enemyHealth > 0) {
+    if (enemyHealth > 0 && playerHealth > 0) {
 
         for (var i = 0; i < wordLength; i++) {
             if (letter === chosenWord.charAt(i)) {
                 //alert("true");
                 underScoreArray[i] = letter.toUpperCase();
                 foundCharacter = true;
+
+                //subtract health in the loop because there may be two of the same letters in the word and health should be subtracted for each occurence.
+                enemyHealth--;
+               
             }
         }
 
         if (!foundCharacter) {
 
-            
+            playerHealth--;
 
             TweenLite.from("#letterPlaceholder", 0.5, {
                 color: "#9f1e1e",
@@ -70,15 +79,17 @@ function checkUserInput(letter) {
                 ease: Bounce.easeOut
             });
 
+            TweenLite.from("body", 0.25, {
+                marginLeft: "20px",
+                ease: Bounce.easeOut
+            });
+
 
 
             currentTile.className += " incorrect";
 
-        } 
-        else 
-        {
-
-            enemyHealth--;
+        }else{
+            //if character was found, play all the animations relevant to the enemy being hurt.
             playHurtAnimation();
             currentTile.className += " correct";
 
@@ -86,12 +97,26 @@ function checkUserInput(letter) {
                 color: "#369f1e",
                 ease: Power2.easeInOut
             });
+        } 
+
+
+
+        if(enemyHealth === 0 && playerHealth != 0){
+            TweenLite.to(enemy, 1, {
+                opacity: "0",
+                ease: Power2.easeInOut
+            });
+            document.getElementById('messageContainer').innerHTML = "You Won!";
+        }else if(enemyHealth != 0 && playerHealth === 0 ){
+            document.getElementById('messageContainer').innerHTML = "You Lost!";
         }
+
 
     }
 
     document.getElementById('letterPlaceholder').innerHTML = underScoreArray.join("  ");
     document.getElementById('health').innerHTML = 'Enemy Health: ' + enemyHealth;
+    document.getElementById('playerHealth').innerHTML = 'Your Health: ' + playerHealth;
     foundCharacter = false;
 
 }
@@ -108,10 +133,15 @@ function playHurtAnimation() {
             '-webkit-filter': 'hue-rotate(230deg)'
         }
     });
+
+    TweenLite.from("#stageBackground", 0.25, {
+        marginLeft: "20px",
+        ease: Bounce.easeOut
+    });
 }
 
 function resetEnemyVisualDefault() {
-    var enemy = document.getElementById('enemy');
+ 
 
     enemy.style.webkitTransform = 'matrix(1, 0, 0, 1, 0, 0)';
     enemy.style.webkitFilter = 'none';

77545830ba71b761c6091ea574a470571b406f69
David Reyes-Cairo(SurfaceBook) <dreyescairo@gmail.com>
2017-11-02 13:10:27 -0600

    animated message success and failed. added click event listeners to the tiles. made it so pressing the same letter again doesnt execute any damage calls.

--- Capstone/js/main.js
+++ Capstone/js/main.js
@@ -1,36 +1,46 @@
 // JavaScript Document
 "use strict";
 
-var words = ["david", "hat", "bat", "computer", "work", "school"];
+var words;
 var chosenWord = "";
 var wordLength = 0;
 var underScoreArray = [];
+var guessedLetters = [];
 var enemyHealth = 0;
 var playerHealth = 0;
 var enemy = document.getElementById('enemy');
 
 function initGame() {
 
+    words = ["david", "hat", "bat", "computer", "work", "school"];
     chosenWord = getRandomWord();
     wordLength = chosenWord.length;
     enemyHealth = wordLength;
-    playerHealth = wordLength - 1;
+    playerHealth = wordLength + 4;
 
     console.log(chosenWord);
 
+    //add the letter placeholders when we get the random word.
     for (var i = 0; i < wordLength; i++) {
         underScoreArray.push("_");
     }
+
+    //add click events to all of the letter tiles on init.
+    for (var i = 0; i < document.getElementsByClassName('tile').length; i++) {
+        document.getElementsByClassName('tile')[i].addEventListener('click', function (event) {
+            checkUserInput(this.id);
+        });
+    }
+
     document.getElementById('letterPlaceholder').innerHTML = underScoreArray.join("  ");
     document.getElementById('health').innerHTML = 'Enemy Health: ' + enemyHealth;
-
     document.getElementById('playerHealth').innerHTML = 'Your Health: ' + playerHealth;
 }
 
 
 function getRandomWord() {
 
-    return words[Math.floor(Math.random() * words.length)];
+    return words[Math.floor(Math.random() * words.length)].toUpperCase();
 }
 
 
@@ -40,80 +50,119 @@ document.onkeyup = function (event) {
     checkUserInput(event.key);
 }
 
+function determineIfLetterWasUsedBefore(_letter) {
+    return guessedLetters.indexOf(_letter) > -1;
+}
+
 function checkUserInput(letter) {
 
+    resetEnemyVisualDefault();
     //TweenLite.killTweensOf("#letterPlaceholder");
     var foundCharacter = false;
     var currentTile = document.getElementById(letter);
     var placeholder = document.getElementById('letterPlaceholder');
     placeholder.style.color = "white"; //reset the color back to default at the beginning of function call.
-    resetEnemyVisualDefault();
+    letter = letter.toUpperCase();
+    var wasLetterUsed = determineIfLetterWasUsedBefore(letter);
 
-
-    letter = letter.toLowerCase();
 
     if (enemyHealth > 0 && playerHealth > 0) {
 
-        for (var i = 0; i < wordLength; i++) {
-            if (letter === chosenWord.charAt(i)) {
-                //alert("true");
-                underScoreArray[i] = letter.toUpperCase();
-                foundCharacter = true;
+        //check if letter was already checked before and added to the list as a correct letter. if not, go ahead and hurt the enemy.
+        if (!wasLetterUsed) {
 
-                //subtract health in the loop because there may be two of the same letters in the word and health should be subtracted for each occurence.
-                enemyHealth--;
-               
+            for (var i = 0; i < wordLength; i++) {
+
+                if (letter === chosenWord.charAt(i)) {
+                    //alert("true");
+                    underScoreArray[i] = letter;
+
+                    enemyHealth--;
+                    foundCharacter = true;
+                }
             }
+
+
+            if (!foundCharacter) {
+
+                playerHealth--;
+
+                TweenLite.from("#letterPlaceholder", 0.5, {
+                    color: "#9f1e1e",
+                    ease: Power2.easeInOut
+                });
+                TweenLite.from("#letterPlaceholder", 0.25, {
+                    marginLeft: "20px",
+                    ease: Bounce.easeOut
+                });
+
+                TweenLite.from("body", 0.25, {
+                    marginLeft: "20px",
+                    ease: Bounce.easeOut
+                });
+
+
+
+                currentTile.className += " incorrect";
+
+            } else {
+                //if character was found, play all the animations relevant to the enemy being hurt.
+                playHurtAnimation();
+                currentTile.className += " correct";
+
+                TweenLite.from("#letterPlaceholder", 0.25, {
+                    color: "#369f1e",
+                    ease: Power2.easeInOut
+                });
+            }
+
         }
-
-        if (!foundCharacter) {
-
-            playerHealth--;
-
-            TweenLite.from("#letterPlaceholder", 0.5, {
-                color: "#9f1e1e",
-                ease: Power2.easeInOut
-            });
-            TweenLite.from("#letterPlaceholder", 0.25, {
-                marginLeft: "20px",
-                ease: Bounce.easeOut
-            });
-
-            TweenLite.from("body", 0.25, {
-                marginLeft: "20px",
-                ease: Bounce.easeOut
-            });
-
-
-
-            currentTile.className += " incorrect";
-
-        }else{
-            //if character was found, play all the animations relevant to the enemy being hurt.
-            playHurtAnimation();
-            currentTile.className += " correct";
-
-            TweenLite.from("#letterPlaceholder", 0.25, {
-                color: "#369f1e",
-                ease: Power2.easeInOut
-            });
-        } 
-
-
-
-        if(enemyHealth === 0 && playerHealth != 0){
-            TweenLite.to(enemy, 1, {
-                opacity: "0",
-                ease: Power2.easeInOut
-            });
-            document.getElementById('messageContainer').innerHTML = "You Won!";
-        }else if(enemyHealth != 0 && playerHealth === 0 ){
-            document.getElementById('messageContainer').innerHTML = "You Lost!";
-        }
-
 
     }
 
+
+    if (enemyHealth === 0 && playerHealth != 0) {
+
+        TweenLite.to(enemy, 1, {
+            opacity: "0",
+            ease: Power2.easeInOut
+        });
+
+        TweenLite.to(".messageTextContainer", 1, {
+            opacity: "0.5",
+            ease: Power2.easeInOut,
+            delay: 0.5
+        });
+
+        TweenLite.to("#messageText", 1, {
+            opacity: "1",
+            ease: Power2.easeInOut,
+            delay: 0.5
+        });
+
+        document.getElementById('messageText').innerHTML = "You Win";
+        document.getElementById('messageText').style.color = "green";
+
+    } else if (enemyHealth != 0 && playerHealth === 0) {
+
+        TweenLite.to(".messageTextContainer", 1, {
+            opacity: "0.5",
+            ease: Power2.easeInOut,
+
+        });
+
+        TweenLite.to("#messageText", 1, {
+            opacity: "1",
+            ease: Power2.easeInOut,
+
+        });
+
+        document.getElementById('messageText').innerHTML = "You Died";
+        document.getElementById('messageText').style.color = "red";
+    }
+
+
+    guessedLetters.push(letter);
     document.getElementById('letterPlaceholder').innerHTML = underScoreArray.join("  ");
     document.getElementById('health').innerHTML = 'Enemy Health: ' + enemyHealth;
     document.getElementById('playerHealth').innerHTML = 'Your Health: ' + playerHealth;
@@ -141,10 +190,10 @@ function playHurtAnimation() {
 }
 
 function resetEnemyVisualDefault() {
- 
 
     enemy.style.webkitTransform = 'matrix(1, 0, 0, 1, 0, 0)';
     enemy.style.webkitFilter = 'none';
 }
+
 
 initGame();
